8c6cc00b57d019ca47336bf1076ecccacdd009c4
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2022-06-06 13:37:23 +0200

    adjust video

--- heat-counter/capturePic.js
+++ heat-counter/capturePic.js
@@ -1,14 +1,14 @@
-let camera_button = document.querySelector("#start-camera");
+let start_camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
-let click_button = document.querySelector("#click-photo");
+let take_photo_button = document.querySelector("#take-photo");
 let canvas = document.querySelector("#canvas");
 
-camera_button.addEventListener('click', async function() {
+start_camera_button.addEventListener('click', async function() {
    	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
 	video.srcObject = stream;
 });
 
-click_button.addEventListener('click', function() {
+take_photo_button.addEventListener('click', function() {
    	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
    	let image_data_url = canvas.toDataURL('image/jpeg');
 
