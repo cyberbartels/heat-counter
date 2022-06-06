fd3f9582bcada345b121b3e6c752ebe57a29be58
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2022-06-06 13:52:04 +0200

    Get canvas blob

--- heat-counter/capturePic.js
+++ heat-counter/capturePic.js
@@ -2,6 +2,8 @@ let start_camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
 let take_photo_button = document.querySelector("#take-photo");
 let canvas = document.querySelector("#canvas");
+let analyze_photo_button = document.querySelector("#analyze-photo");
+let result_selector = document.querySelector("#select-result");
 
 start_camera_button.addEventListener('click', async function() {
    	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
@@ -14,4 +16,22 @@ take_photo_button.addEventListener('click', function() {
 
    	// data url of the image
    	console.log(image_data_url);
+});
+
+analyze_photo_button.addEventListener('click', async function() {
+    result_selector.innerHTML = "";
+    canvas.toBlob(blob=> {
+        console.log(blob);
+
+        let options = ["a1", "b2", "c3", "d4", "e5"]; 
+        for(var i = 0; i < options.length; i++) {
+            var opt = options[i];
+        
+            var el = document.createElement("option");
+            el.text = opt;
+            el.value = opt;
+        
+            select.add(el);
+        }​
+    });
 });
