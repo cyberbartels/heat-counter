f19faf978315ecfe64d47e0bcc841f18430ab1bd
cyberbartels <71546296+cyber-bartels@users.noreply.github.com>
2022-06-06 21:03:53 +0200

    WIP

--- heat-counter/capturePic.js
+++ heat-counter/capturePic.js
@@ -5,36 +5,36 @@ let canvas = document.querySelector("#canvas");
 let analyze_photo_button = document.querySelector("#analyze-photo");
 let result_selector = document.querySelector("#select-result");
 
-start_camera_button.addEventListener('click', async function() {
-   	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-	video.srcObject = stream;
+const getBase64StringFromDataURL = (dataURL) =>
+    dataURL.replace('data:', '').replace(/^.+,/, '');
+
+start_camera_button.addEventListener('click', async function () {
+    let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+    video.srcObject = stream;
 });
 
-take_photo_button.addEventListener('click', function() {
-   	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-   	let image_data_url = canvas.toDataURL('image/jpeg');
+take_photo_button.addEventListener('click', function () {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    let image_data_url = canvas.toDataURL('image/jpeg');
 
-   	// data url of the image
-   	console.log(image_data_url);
+    // data url of the image
+    console.log(image_data_url);
 });
 
-analyze_photo_button.addEventListener('click', async function() {
-    canvas.toBlob(blob=> {
-        console.log(blob);
+analyze_photo_button.addEventListener('click', async function () {
+    let image_data_url = canvas.toDataURL('image/jpeg');
+    console.log(image_data_url);
+    console.log(getBase64StringFromDataURL(image_data_url));
 
-        let base64 = getBase64StringFromDataURL(blob);
-        console.log(base64);
-        
-        // result_selector.innerHTML = "";
-        // let options = ["a1", "b2", "c3", "d4", "e5"]; 
-        // for(var i = 0; i < options.length; i++) {
-        //     var opt = options[i];
-        
-        //     var el = document.createElement("option");
-        //     el.text = opt;
-        //     el.value = opt;
-        
-        //     result_selector.add(el);
-        // };​
-    });
+    result_selector.innerHTML = "";
+    let options = ["a1", "b2", "c3", "d4", "e5"]; 
+    for(var i = 0; i < options.length; i++) {
+        var opt = options[i];
+
+        var el = document.createElement("option");
+        el.text = opt;
+        el.value = opt;
+
+        result_selector.add(el);
+    };
 });
